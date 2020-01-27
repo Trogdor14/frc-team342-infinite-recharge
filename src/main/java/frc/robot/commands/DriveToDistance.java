3dd96b73c4eda97f18b497dcacb98a062e28b326
Emma McBride <emcbride@342.lan>
2020-01-27 17:33:48 -0500

    fixed automove code.

--- src/main/java/frc/robot/commands/DriveToDistance.java
+++ src/main/java/frc/robot/commands/DriveToDistance.java
@@ -2,6 +2,7 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Factory;
 import frc.robot.Robot;
@@ -28,8 +29,10 @@ public class DriveToDistance extends CommandBase {
   private double current_R2;
   private double current_Left;
 	private double current_Right; 
-	private double left_rotation_count;
-	private double right_rotation_count;
+	private double L1_rotation_count;
+  private double L2_rotation_count;
+  private double R1_rotation_count;
+  private double R2_rotation_count;
 
 	//private double degrees_off_zero;
 	private double left_speed;
@@ -62,16 +65,37 @@ public class DriveToDistance extends CommandBase {
     current_R1 = driveSystem.encoderR1.getPosition() - init_R1;
     current_R2 = driveSystem.encoderR2.getPosition() - init_R2;
 
+    L1_rotation_count = Math.abs(current_L1 / 4096); 
+    L2_rotation_count = Math.abs(current_L2 / 4096); 
+    R1_rotation_count = Math.abs(current_R1 / 4096); 
+    R2_rotation_count = Math.abs(current_R2 / 4096); 
+
+    //might need to do some testing to see which side is inverted
+    driveSystem.Drive(left_speed, right_speed *1, 0); 
+
+    SmartDashboard.putNumber("left1", L1_rotation_count);
+    SmartDashboard.putNumber("left2", L2_rotation_count);
+    SmartDashboard.putNumber("right1", R1_rotation_count);
+    SmartDashboard.putNumber("right2", R2_rotation_count);
 
   }
 
   @Override
   public void end(boolean interrupted) {
-    driveSystem.stopDrive; 
+    driveSystem.stopDrive(); 
   }
 
   @Override
   public boolean isFinished() {
-    return false;
+    /*if((L1_rotation_count >= goal && L2_rotation_count >= goal) || (R1_rotation_count >= goal && R2_rotation_count >= goal)){
+      return true;
+    } else {
+      return false;
+    }*/
+
+    //kurnnell likes to be difficult so heres this new if statment @cnonnell 
+    return ((L1_rotation_count >= goal && L2_rotation_count >= goal) || (R1_rotation_count >= goal && R2_rotation_count >= goal) ? true : false);
   }
+
 }
+
